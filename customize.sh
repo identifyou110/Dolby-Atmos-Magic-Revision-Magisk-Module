4e6afc76c339d8fc21bb8b1ea07fbdd5456e9cd5
Rei Ryuki <46316533+reiryuki@users.noreply.github.com>
2022-05-14 04:27:35 +0700

    Enhancements

--- customize.sh
+++ customize.sh
@@ -83,19 +83,6 @@ rm -rf /data/unencrypted/magisk/$MODID
 rm -rf /cache/magisk/$MODID
 ui_print " "
 
-# power save
-PROP=`getprop power.save`
-FILE=$MODPATH/system/etc/sysconfig/*
-if [ "$PROP" == 1 ]; then
-  ui_print "- $MODNAME will not be allowed in power save."
-  ui_print "  It may save your battery but decreasing $MODNAME performance."
-  for PKGS in $PKG; do
-    sed -i "s/<allow-in-power-save package=\"$PKGS\"\/>//g" $FILE
-    sed -i "s/<allow-in-power-save package=\"$PKGS\" \/>//g" $FILE
-  done
-  ui_print " "
-fi
-
 # function
 conflict() {
 for NAMES in $NAME; do
